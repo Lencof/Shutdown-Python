c9821f0141985fd2c16a8ec75a063386094851f5
Lencof <71154457+Lencof@users.noreply.github.com>
2021-02-06 09:44:01 +0300

    Update Shutdown Modified №3.py

--- Shutdown Modified №3.py
+++ Shutdown Modified №3.py
@@ -1,1 +1,32 @@
-gh
+# __Author__ __Lencof__
+# Shutdown Modified №3.py
+# A new Shutdown is created this code turns off the computer and after shutdown creates a file with a note on the desktop
+
+import os
+os.system('shutdown -s -t 0')
+
+README = '''
+Shutdown done
+'''
+
+# Open for 'w'riting
+f = open('README.txt', 'w')
+# Write text to file
+f.write(README)
+# Close the file
+f.close()
+
+# If no mode is specified
+# 'r'ead mode is assumed by default
+f = open('README.txt')
+while True: # use True
+  line = f.readline()
+  # Zero length indicates EOF
+  if len(line) == 0:
+    break
+  # The 'line' already has a newline
+  # at the end of each line
+  # ssince it is reading from a file.
+  print(line, end='')
+# close the file
+f.close()
